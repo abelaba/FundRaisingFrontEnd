f8954a47f15d759ba1c4828c7cb640669fa2a8a5
Mercy Habte <61546403+Mercy-habte@users.noreply.github.com>
2021-05-05 10:30:54 +0300

    Working on Floating and Content js

---
+++ src/components/Content.js
@@ -0,0 +1,13 @@
+import MainPic from './MainPic';
+import Menu from './Menu';
+
+const Content = () => {
+    return (
+        <div className="content">
+            <MainPic />
+            <Menu />
+        </div>
+    )
+}
+
+export default Content
---
+++ src/components/Floating.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Floating = () => {
+    return (
+        <div className="display">
+        <h3>Help Others</h3>
+        <p className="intro">
+        Crowdfunding is the process by which individuals and organizations raise money
+         from a large number of people. 
+   “It is more rewarding to watch money change the world than to watch it accumulate.” 
+         
+        </p>
+      </div>
+    )
+}
+
+export default Floating
