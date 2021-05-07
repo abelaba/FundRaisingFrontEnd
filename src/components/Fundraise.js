3fa0ed8cc3a622bf9ade6a58e3446d4db55aa85b
Mercy Habte <61546403+Mercy-habte@users.noreply.github.com>
2021-05-07 22:31:50 +0300

    Working of Fundraisings

---
+++ src/components/Fundraise.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Fundraise = () => {
+    return (
+        <div className="fundraise">
+        <form action=""> 
+            <h3>category Goes here</h3>
+            <h2>Title Goes Here</h2>
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+             Minima quia quisquam rerum suscipit eius minus temporibus,
+              facere, voluptates quidem tempora aperiam non at officiis voluptatem 
+              eveniet illum obcaecati, cum velit.</p>
+         <input type="number" hidden />
+         <button type="submit" className="donate-btn">Donate</button>
+         
+        </form>
+     </div>
+    )
+}
+
+export default Fundraise
