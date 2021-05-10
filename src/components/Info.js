658d56fc47e2c177d0d198c759c053acde00812c
Mercy Habte <61546403+Mercy-habte@users.noreply.github.com>
2021-05-10 20:32:46 +0300

    Working on the Info status

---
+++ src/components/Info.js
@@ -0,0 +1,23 @@
+const Info = () => {
+    return (
+        <div className="about">
+        <h2>Help With Us</h2>
+        <p>
+        People can use Erdata for themselves, friends and family, or even complete strangers in random
+         acts of kindness. You can raise money for medical expenses
+        , education costs, volunteer programs, youth sports, funerals & memorials, etc   
+            <ul className="ulcenter">
+                <li><h3>Secure Payement</h3></li>
+                <li> <h3>Withdraw Anytime</h3></li>
+                <li><h3>Easey to Use</h3></li>
+            </ul>
+        </p>
+       
+
+          
+
+    </div>
+    )
+}
+
+export default Info
